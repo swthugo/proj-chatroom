a528edac8ee3b902420d06972a841de3cadaed53
Hugo Siu (MIS) <hugosiu@cms.hkcable.com>
2025-02-19 17:03:03 +0800

    add files

--- backend/src/main/java/dev/hugosiu/Chatbot/ChatbotApplication.java
+++ backend/src/main/java/dev/hugosiu/Chatbot/ChatbotApplication.java
@@ -1,13 +1,21 @@
 package dev.hugosiu.Chatbot;
 
+import lombok.extern.log4j.Log4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+@Log4j
 @SpringBootApplication
 public class ChatbotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatbotApplication.class, args);
-	}
-
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            log.info("message from application.properties" + environment.getProperty("message-from-application-properties"));
+        };
+    }
 }
